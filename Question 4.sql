51c1fa2515ea87bb900e5218173fc4806b702441
ArjunLuam <arjunluam0732@gmail.com>
2024-09-24 12:14:23 +0530

    Q4 & 5 added

---
+++ Question 4.sql
@@ -0,0 +1,47 @@
+Several friends at a cinema ticket office would like to reserve consecutive available seats.
+query all the consecutive seats order by the seat_id using the following cinema table
+
+DDL & DML QUERIES:
+CREATE TABLE cinema (
+ seat_id INT PRIMARY KEY,
+ free int
+);
+delete from cinema;
+INSERT INTO cinema (seat_id, free) VALUES (1, 1);
+INSERT INTO cinema (seat_id, free) VALUES (2, 0);
+INSERT INTO cinema (seat_id, free) VALUES (3, 1);
+INSERT INTO cinema (seat_id, free) VALUES (4, 1);
+INSERT INTO cinema (seat_id, free) VALUES (5, 1);
+INSERT INTO cinema (seat_id, free) VALUES (6, 0);
+INSERT INTO cinema (seat_id, free) VALUES (7, 1);
+INSERT INTO cinema (seat_id, free) VALUES (8, 1);
+INSERT INTO cinema (seat_id, free) VALUES (9, 0);
+INSERT INTO cinema (seat_id, free) VALUES (10, 1);
+INSERT INTO cinema (seat_id, free) VALUES (11, 0);
+INSERT INTO cinema (seat_id, free) VALUES (12, 1);
+INSERT INTO cinema (seat_id, free) VALUES (13, 0);
+INSERT INTO cinema (seat_id, free) VALUES (14, 1);
+INSERT INTO cinema (seat_id, free) VALUES (15, 1);
+INSERT INTO cinema (seat_id, free) VALUES (16, 0);
+INSERT INTO cinema (seat_id, free) VALUES (17, 1);
+INSERT INTO cinema (seat_id, free) VALUES (18, 1);
+INSERT INTO cinema (seat_id, free) VALUES (19, 1);
+INSERT INTO cinema (seat_id, free) VALUES (20, 1);
+
+
+Output :
+
+Sno	seat_id
+1	3
+2	4
+3	5
+4	7
+5	8
+6	14
+7	15
+8	17
+9	18
+10	19
+11	20
+
+
---
+++ Question 5.sql
@@ -0,0 +1,42 @@
+The cancellation rate is computed by dividing the number of canceled (by client or driver) requests with unbanned users by the total number of requests with unbanned users on that day.
+Write a solution to find the cancellation rate of requests with unbanned users (both client and driver must not be banned) each day between "2013-10-01" and "2013-10-03". Round Cancellation Rate to two decimal points.
+Return the result table in any order.
+The result format is in the following example.
+
+DDL & DML QUERY:
+CREATE TABLE User (
+ users_id INT PRIMARY KEY,
+ banned VARCHAR(3) CHECK (banned IN ('Yes', 'No')),
+ role VARCHAR(10) CHECK (role IN ('client', 'driver'))
+);
+CREATE TABLE Trips (
+ id INT PRIMARY KEY,
+ client_id INT,
+ driver_id INT,
+ city_id INT,
+ status VARCHAR(50),
+ request_at DATE,
+ FOREIGN KEY (client_id) REFERENCES User(users_id),
+ FOREIGN KEY (driver_id) REFERENCES User(users_id)
+);
+INSERT INTO User (users_id, banned, role) VALUES
+(1, 'No', 'client'),
+(2, 'Yes', 'client'),
+(3, 'No', 'client'),
+(4, 'No', 'client'),
+(10, 'No', 'driver'),
+(11, 'No', 'driver'),
+(12, 'No', 'driver'),
+(13, 'No', 'driver');
+
+INSERT INTO Trips (id, client_id, driver_id, city_id, status, request_at) VALUES
+(1, 1, 10, 1, 'completed', '2013-10-01'),
+(2, 2, 11, 1, 'cancelled_by_driver', '2013-10-01'),
+(3, 3, 12, 6, 'completed', '2013-10-01'),
+(4, 4, 13, 6, 'cancelled_by_client', '2013-10-01'),
+(5, 1, 10, 1, 'completed', '2013-10-02'),
+(6, 2, 11, 6, 'completed', '2013-10-02'),
+(7, 3, 12, 6, 'completed', '2013-10-02'),
+(8, 2, 12, 12, 'completed', '2013-10-03'),
+(9, 3, 10, 12, 'completed', '2013-10-03'),
+(10, 4, 13, 12, 'cancelled_by_driver', '2013-10-03');
